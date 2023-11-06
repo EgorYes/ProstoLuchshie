3b98b71dcc1252af43594b6dc94a4dcbb2948270
EgorYes <147613394+EgorYes@users.noreply.github.com>
2023-11-07 00:22:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,77 +1,77 @@
-﻿#include<iostream>
+#include<iostream>
 #include<vector>
+#include<string>
 using namespace std;
 
 
 int main() {
+	while (true) {
 
-	double w, min, max, sum, result, aver; int num, size = 1; vector<int> vect;
-	max = 0; result = 0; aver = 0;
 
-	size = 0;
+		double w, min, max, sum, result, aver; int number, size = 1; vector<int> vect;
+		max = 0; result = 0; aver = 0; string num;
 
-	while (true)
-	{
-		cin >> num;
+		size = 0;
 
-		if (num / 1 < 1)
+		cout << "To stop typing, write 'stop'\n";
+
+		while (true)
 		{
-			if (size < 1 ) 
+			cin >> num;
+
+			if (num == "stop")
 			{
-			  cout << "There are no numbers entered." << endl;
-			  return 1;
-		    }
-			break;
+				break;
+			}
+
+			number = stoi(num);
+			vect.push_back(number);
+
+
+			size = vect.size();
+		}
+		min = size;
+
+		for (int p = 0; p < size; p++)
+		{
+			if (vect[p] < min) {
+				min = vect[p];
+			}
 		}
 
-		vect.push_back(num);
+		cout << "Minimum number: " << min << endl;
 
-
-		size = vect.size();
-	}
-	min = size;
-
-	for (int p = 0; p < size; p++)
-	{
-		if (vect[p] < min) {
-			min = vect[p];
+		for (int p = 0; p < size; p++)
+		{
+			if (vect[p] > max) {
+				max = vect[p];
+			}
 		}
-	}
 
-	cout << "Minimum number: " << min << endl;
+		cout << "Maximum number: " << max << endl;
+		sum = 0;
 
-	for (int p = 0; p < size; p++)
-	{
-		if (vect[p] > max) {
-			max = vect[p];
+		for (int i = 0; i < size; i++)
+		{
+			sum += vect[i];
 		}
+
+		cout << "The amount of the number: " << sum << endl;
+		aver = sum / size;
+
+		cout << "Average value: " << aver << endl;
+
+		for (int i = 0; i < size; i++)
+		{
+			w = vect[i] - aver;
+			result -= w;
+		}
+		double resultF = result / size;
+
+		double x = resultF * resultF;
+
+		cout << "Standard deviation: " << x << endl;
+		cout << "" << endl;
+
 	}
-
-	cout << "Maximum number: " << max << endl;
-	sum = 0;
-
-	for (int i = 0; i < size; i++)
-	{
-		sum += vect[i];
-	}
-
-	cout << "The amount of the number: " << sum << endl;
-	aver = sum / size;
-
-	cout << "Average value: " << aver << endl;
-
-	for (int i = 0; i < size; i++)
-	{
-		w = vect[i] - aver;
-		result -= w;
-	}
-	double resultF = result / size;
-
-	double x = resultF * resultF;
-
-	cout << "Standard deviation: " << x << endl;
-
-	system("pause");
-	return 0;
-
 }
